ff6d8058ac1d19feb881d4962355d94d445ab54b
sergei <example@mail.ru>
2021-10-11 21:08:51 +0300

    Create file php and add logic post form

--- src/js/script.js
+++ src/js/script.js
@@ -101,10 +101,10 @@ const modalCloseBtn = document.querySelector("[data-close]")
 function modalOpen() {
 	modal.style.display = "block";
 	document.body.style.overflow = "hidden";
-	clearInterval(modalTimer); 
+	// clearInterval(modalTimer); 
 };
 
-const modalTimer = setTimeout(modalOpen, 5000);
+// const modalTimer = setTimeout(modalOpen, 5000);
 
 modalTrigger.forEach( btn => {
 	btn.addEventListener("click", modalOpen);
@@ -143,78 +143,138 @@ function showModalByScroll() {
 window.addEventListener("scroll", showModalByScroll);
 
 
-    // ////////////Class Card//////////////
+/////////////////// Class Card ///////////////////
 
-    // class MenuCard {
-    //     constructor(src, alt, title, descr, price, parentSelector, ...classes) {
-    //         this.src = src;
-    //         this.alt = alt;
-    //         this.title = title;
-    //         this.descr = descr;
-    //         this.price = price;
-    //         this.classes = classes;
-    //         this.parent = document.querySelector(parentSelector);
-    //         this.transfer = 73;
-    //         this.changeToRu();
-    //     }
+class CardMenu {
+	constructor(src, alt, title, description, price, parentSelector, ...classes) { // ...classes -  колличество дополнительных классов
+		this.src = src;
+		this.alt = alt;
+		this.title = title;
+		this.description = description;
+		this.price = price;
+		this.classes = classes; // array
+		this.parent = document.querySelector(parentSelector);
+		this.transfer = 74;
+		this.changeToRubl();
+	}
 
-    //     changeToRu() {
-    //         this.price = +this.price * this.transfer;
-    //     }
+	changeToRubl() {
+		this.price = +this.price * this.transfer;
+	}
 
-    //     render() {
-    //         const element = document.createElement('div');
-    //         if (this.classes.length === 0) {
-    //             this.element = 'menu__item';
-    //             element.classList.add(this.element);
-    //         } else {
-    //             this.classes.forEach(className => element.classList.add(className));
-    //         }
-    //         element.innerHTML = `
-    //                 <img src=${this.src} alt=${this.alt}>
-    //                 <h3 class="menu__item-subtitle">${this.title}</h3>
-    //                 <div class="menu__item-descr">${this.descr}</div>
-    //                 <div class="menu__item-divider"></div>
-    //                 <div class="menu__item-price">
-    //                     <div class="menu__item-cost">Цена:</div>
-    //                     <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
-    //                 </div>       
-    //         `;
-    //         this.parent.append(element);
-    //     }
-    // }
+	render() {
+		const element = document.createElement("div");
 
-    // new MenuCard(
-    //     "img/tabs/vegy.jpg",
-    //     "vegy",
-    //     'Меню "Фитнес"',
-    //     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    //     9,
-    //     ".menu .container",
-    //     "menu__item"
-    // ).render(); // данный синтаксис используется если нужно использовать класс один раз
+		if (this.classes.length === 0) {
+			this.element = "menu__item";
+			element.classList.add(this.element);
+		} else {
+			this.classes.forEach(className => element.classList.add(className));
+		} 
 
-    // new MenuCard(
-    //     "img/tabs/elite.jpg",
-    //     "elite",
-    //     'Меню "Премиум"',
-    //     'В меню "Премиум" мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    //     12,
-    //     ".menu .container",
-    //     "menu__item"
-    // ).render();
+		element.innerHTML = `
+				<img src=${this.src} alt=${this.alt}>
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+					<div class="menu__item-descr">${this.description}</div>
+					<div class="menu__item-divider"></div>
+						<div class="menu__item-price">
+								<div class="menu__item-cost">Цена:</div>
+								<div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+						</div>
+		`;
 
-    // new MenuCard(
-    //     "img/tabs/post.jpg",
-    //     "post",
-    //     'Меню "Постное"',
-    //     'Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    //     16,
-    //     ".menu .container",
-    //     "menu__item"
-    // ).render();
+		this.parent.append(element);
+	}
+}
 
+    new CardMenu (
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container",
+        // не добавляем классы
+    ).render(); // данный синтаксис используется если нужно использовать класс один раз
 
+    const vipMenu = new CardMenu (
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню "Премиум"',
+        'В меню "Премиум" мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        12,
+        ".menu .container",
+        "menu__item"
+    );
+		vipMenu.render();
+
+    const leanMenu = new CardMenu (
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        16,
+        ".menu .container",
+        "menu__item"
+    );
+		leanMenu.render();
+
+/////////////////// Forms ////////////////////
+
+const forms = document.querySelectorAll('form');
+
+const message = {
+	loading: 'Загрузка',
+	success: 'Спасибо! Мы скоро свяжемся с вами',
+	failuer: 'Что-то пошло не так... о_О',
+}
+
+forms.forEach(form => {
+	postData(form);
+});
+
+function postData(form) {
+	form.addEventListener('submit', (event) => {
+		event.preventDefault();
+
+		const statusMessage = document.createElement('div');
+		statusMessage.classList.add('status');
+		statusMessage.textContent = message.loading;
+		form.append(statusMessage);
+
+		const request = new XMLHttpRequest();
+		request.open('POST', 'server.php');
+
+		// request.setRequestHeader('Content-type', 'multipart/form-data') если использовать данный заголовок
+		// в связке с XMLHttpRequest(), то будет возникать ошибка. Заголовок проставиться автоматически
+		// formData используется в php
+		request.setRequestHeader('Content-type', 'application/json');
+		const formData = new FormData(form); // Всегда проверять наличие атрибута "name" у input
+
+		const object = {};
+		formData.forEach(function(value, key) { // Перебираем formData т.к. его нельзя передать в JSON
+			object[key] = value;
+		});
+
+		const json = JSON.stringify(object); // Конвертируем в JSON
+
+		request.send(json);
+
+		request.addEventListener('load', () => {
+			if (request.status === 200) {
+				console.log(request.response);
+				statusMessage.textContent = message.success;
+				form.reset();
+				setTimeout(() => {
+					statusMessage.remove();
+				}, 5000);
+				
+			} else {
+				statusMessage.textContent = message.failuer;
+			}
+		});
+	});
+}
 
 
 
